c8f35475836c1d6cfd8b60160c71873cb05d055a
Moe <chughtai.mh@gmail.com>
2016-12-17 15:03:15 -0500

    added dynamic directory detection

--- webpages/homepage.py
+++ webpages/homepage.py
@@ -1,8 +1,14 @@
 # -*- coding: UTF-8 -*-
 #!/usr/bin/env python
 """Contains code for homepage request handling"""
+import os
+import sys
+
+CURRENT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(CURRENT_DIRECTORY + '/')
+
 
 def homepage(*args, **kwargs):
-    """An HTTP request handler that returns the homepage"""
-
-    return 'Welcome'
+	"""An HTTP request handler that returns the homepage"""
+	with open('home.html', 'r') as f:
+		return f.read()
